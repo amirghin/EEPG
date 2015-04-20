d7a41199e848a84e5bd397103013075cf89e943f
amirghin <adrian@aastorga.com>
2015-04-19 23:44:05 -0600

    agregando ; a conexion

--- class/conexion.php
+++ class/conexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "sys_nutrivital";
 $db = "nutrivital";
-$password ='nutrivital' 
+$password ='nutrivital';
 
 $conexion = mysqli_connect($servername, $username, $password, $db);
 
