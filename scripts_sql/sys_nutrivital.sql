0429fece68072840af37b75fe3f8e9567f3d2f48
amirghin <adrian@aastorga.com>
2015-04-19 23:45:25 -0600

    arreglando script

--- scripts_sql/sys_nutrivital.sql
+++ scripts_sql/sys_nutrivital.sql
@@ -1,6 +1,6 @@
 CREATE USER sys_nutrivital@localhost IDENTIFIED BY 'nutrivital';
-GRANT DELETE ON nutrivital.* TO sys_videoclub@localhost;
-GRANT SELECT ON nutrivital.* TO sys_videoclub@localhost;
-GRANT INSERT ON nutrivital.* TO sys_videoclub@localhost;
-GRANT UPDATE ON nutrivital.* TO sys_videoclub@localhost;
+GRANT DELETE ON nutrivital.* TO sys_nutrivital@localhost;
+GRANT SELECT ON nutrivital.* TO sys_nutrivital@localhost;
+GRANT INSERT ON nutrivital.* TO sys_nutrivital@localhost;
+GRANT UPDATE ON nutrivital.* TO sys_nutrivital@localhost;
 FLUSH PRIVILEGES;
