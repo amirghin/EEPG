20d622c9b71f8d2e825a34b96c32a97d5ecc8746
hocampo <hocampo@publicisgroupe.net>
2015-04-20 01:05:40 -0600

    busqueda control

---
+++ busquedas/busqueda_control.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+ require "../class/conexion.php";
+
+
+
+	$id_paciente = $_POST["id_paciente"];
+	$query = "SELECT * FROM controles WHERE pacientes_id LIKE '%{$id_paciente}%'";
+	$resultado = mysqli_query($conexion, $query);
+
+	$array = array();
+
+	while($row=mysqli_fetch_assoc($resultado)){
+	$array[] = $row;
+
+	}
+
+	echo '{"controles":'.json_encode($array).'}';
+
+
+?>
