3f1cb0fc2b0c9fa578247c00901fe9618c097e54
Fabiola <fab.tames@gmail.com>
2015-04-20 00:31:18 -0600

    Agregando comments

--- js/main.js
+++ js/main.js
@@ -9,10 +9,9 @@ function busqueda_paciente(){
         });
         busqueda.done(function(response){
             var object = jQuery.parseJSON(response);
-            //$("#result").html("");
             var table = "<tr><td>Id Paciente</td><td>Nombre</td><td>Apellido</td><td>Genero</td><td>Fecha Ingreso</td><td>Fecha nacimiento</td> <td>Talla</td><td>Peso Meta</td><td>Circunferencia de la muneca</td><td>Antecedentes personales</td> <td>Padecimientos familiares</td> <td>Nombre usuario</td>  <td>Modificar</td> <td>Eliminar</td></tr> ";
             var tableValues = "";
-            $.each(object.pacientes, function(key,value){
+            $.each(object.pacientes, function(key,value){  // funcion para recorrer el objeto JSON parseado
 
                 tableValues += "<tr><td>"+value.pacientes_id+"</td><td>"+value.pacientes_nombre+"</td><td>"+value.pacientes_apellidos+"</td><td>"+value.pacientes_genero+"</td> <td>"+value.pacientes_fecha+"</td> <td>"+value.pacientes_fecha_nac+"</td> <td>"+value.pacientes_talla+"</td> <td>"+value.pacientes_peso_meta+"</td> <td>"+value.pacientes_circ_muneca+"</td> <td>"+value.pacientes_ant_personales+"</td> <td>"+value.pacientes_padec_familiares+"</td><td>"+value.usuarios_nombre+"</td><td> <input type='button' value='Eliminar'/> </td> <td> <input type='button' value='Modificar'/> </td><td><input type='hidden' value="+ value.pacientes_id+"></td></tr>";
             });
