86c7a649be002c3478f12f81e61e651456a7fea8
Fabiola <fab.tames@gmail.com>
2015-04-19 23:29:39 -0600

    Busqueda pacientes

--- class/busqueda_pacientes.php
+++ class/busqueda_pacientes.php
@@ -1,6 +1,23 @@
 <?php
 
-$nombre_paciente = $_POST[""];
+
+
+ require "conexion.php";
+
+
+
+	$nombre_paciente = $_POST["nombre_paciente"];
+	$query = "SELECT * FROM pacientes WHERE pacientes_nombre LIKE '%{$nombre_paciente}%'";
+	$resultado = mysqli_query($conexion, $query);
+
+	$array = array();
+
+	while($row=mysqli_fetch_assoc($resultado)){
+	$array[] = $row;
+
+	}
+
+	echo '{"pacientes":'.json_encode($array).'}';
 
 
 ?>
--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 function busqueda_paciente(){
+    var buscar = $("#nombre_paciente").val();
 
+    if (buscar!=""){
+        var busqueda = $.ajax({
+            url: "class/busqueda_pacientes.php", // PHP que se ejecuta en el click del boton
+            type: "POST",
+            data: {nombre_paciente:buscar}, //Datos que se envian al PHP por medio del POST
+        });
+        busqueda.done(function(response){
+            var object = jQuery.parseJSON(response);
+            console.log(object);
+            var table = "<tr><td>Nombre</td><td>Apellido</td><td>Nombre de usuario</td></tr>";
+            var tableValues = "";
+            $.each(object.usuarios, function(key,value){
+                tableValues += "<tr><td>"+value.pacientes_nombre+"</td><td>"+value.pacientes_apellido+"</td><td>"+value.nom_usuario+"</td><td> <input type='button' value='Eliminar'/> </td> <td> <input type='button' value='Modificar'/> </td></tr>";
+                
+            });
+            $("#result").html(table+tableValues);
+        });
+        busqueda.error(function(error){
+            alert("error");
+        });
+
+    }
 }
 
 
 $(document).ready(function(){
 
 	$("#buscar_pacientes").on("click", function(){
-		console.log("sirvee!!!");
+		busqueda_paciente();
 	})
 
-
-   
     /**** BTN TAB USUARIO *****/
     $(".nextTab").click(function(){
         $('.tabUsuario').tab('show');
