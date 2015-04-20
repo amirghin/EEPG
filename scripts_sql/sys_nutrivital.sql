7cff0e1d7c2341f6d9bd825367c5386c2db8b179
amirghin <adrian@aastorga.com>
2015-04-20 15:03:46 -0600

    borrando archivos

--- scripts_sql/sys_nutrivital.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER sys_nutrivital@localhost IDENTIFIED BY 'nutrivital';
-GRANT DELETE ON nutrivital.* TO sys_nutrivital@localhost;
-GRANT SELECT ON nutrivital.* TO sys_nutrivital@localhost;
-GRANT INSERT ON nutrivital.* TO sys_nutrivital@localhost;
-GRANT UPDATE ON nutrivital.* TO sys_nutrivital@localhost;
-FLUSH PRIVILEGES;
